efcad20fee2d03d66bdcf4f1cd0504474072a3cb
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2026-04-19 13:53:04 +0200

    feat: the `BB84.EPG.Downloader` added

---
+++ src/BB84.EPG.Downloader/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.IO.Compression;
+using System.Text;
+
+namespace BB84.EPG.Downloader;
+
+internal class Program
+{
+	private static void Main(string[] args)
+	{
+		if (args.Length != 1)
+		{
+			Console.WriteLine("Usage: BB84.EPG.Downloader.exe <download_path>");
+			return;
+		}
+
+		if (Directory.Exists(args[0]) == false)
+		{
+			Console.WriteLine($"The specified directory does not exist: {args[0]}");
+			return;
+		}
+
+		string downloadPath = args[0];
+
+		DownloadEPG("https://iptv-epg.org/files/epg-de.xml", downloadPath);
+	}
+
+	private static void DownloadEPG(string epgUrl, string downloadPath)
+	{
+		using HttpClient httpClient = new();
+		string epgFileName = epgUrl.Split('/')
+			.Last()
+			.Replace("-de", string.Empty);
+		string epgFilePath = Path.Combine(downloadPath, epgFileName + ".gz");
+
+		string epgContent = httpClient.GetStringAsync(epgUrl)
+			.GetAwaiter()
+			.GetResult();
+
+		epgContent = epgContent.Replace("DE - ", string.Empty);
+
+		byte[] bytes = Encoding.UTF8.GetBytes(epgContent);
+
+		using MemoryStream inputStream = new(bytes);
+		using FileStream outputStream = File.Create(epgFilePath);
+		using GZipStream gzip = new(outputStream, CompressionMode.Compress);
+
+		inputStream.CopyTo(gzip);
+
+		inputStream.Close();
+		gzip.Close();
+		outputStream.Close();
+	}
+}
